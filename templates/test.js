73c3ad4ff15d4ac23ed8bfef7f5ff138e94f6ba5
Coderyesbank <164654567+Coderyesbank@users.noreply.github.com>
2024-09-08 22:58:12 +0530

    Add files via upload

---
+++ templates/test.js
@@ -0,0 +1,31 @@
+let btn1=document.querySelector("#nl1");
+let mylink1=document.querySelector("#link_it1");
+btn1.addEventListener("click",btn1Func);
+function btn1Func()
+{
+    mylink1.click();
+}
+
+let btn2=document.querySelector("#nl2");
+let mylink2=document.querySelector("#link_it2");
+btn2.addEventListener("click",btn2Func);
+function btn2Func()
+{
+    mylink2.click();
+}
+
+let btn3=document.querySelector("#nl3");
+let mylink3=document.querySelector("#link_it3");
+btn3.addEventListener("click",btn3Func);
+function btn3Func()
+{
+    mylink3.click();
+}
+
+let bdy_Btn2=document.querySelector("#bdy_btn2");
+let myBlink2=document.querySelector("#blink_it2");
+bdy_Btn2.addEventListener("click",bdy2Func);
+function bdy2Func()
+{
+    myBlink2.click();
+}
